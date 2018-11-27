7b7503ae6b4cbe8a671f228d3621cd4f0551797b
Jack O'Shea <jackosx@users.noreply.github.com>
2018-11-27 14:13:53 -0500

    Created modules for making function calls on song beats

---
+++ PythonSketches/metronome.py
@@ -0,0 +1,52 @@
+import queue
+import timers
+
+ticks = 0
+frequency = 32 # 32 ticks per beat
+meter = 4
+
+prev_4 = -1
+prev_8 = -1
+prev_16 = -1
+
+q4  = queue.Queue()
+q8  = queue.Queue()
+q16 = queue.Queue()
+
+items = ()
+
+def on_next_beat(f, *args, **kwargs):
+    q4.put((f, args, kwargs))
+
+def hit_beat():
+    global prev_4
+    prev_4 = count_beat()
+    while not q4.empty():
+        global items
+        items = q4.get_nowait()
+        f, args, kwargs = items[0], items[1], items[2]
+        f(*args)
+
+def hit_eighth():
+    global prev_8
+    prev_8 = count_8()
+
+
+def tick():
+    global ticks
+    ticks = ticks + 1
+    if prev_4 != count_beat(): # nest these?
+        hit_beat()
+    if prev_8 != count_8():
+            hit_eighth()
+
+def count_16():
+    return (ticks // 8) % 8
+
+def count_8():
+    return (ticks // 16) % 4
+
+def count_beat():
+    return (ticks // 32) % 4
+
+ticker = timers.MeteredTimer(120, frequency, tick)
---
+++ PythonSketches/timers.py
@@ -0,0 +1,58 @@
+
+
+from threading import Timer
+
+# https://stackoverflow.com/questions/474528/what-is-the-best-way-to-repeatedly-execute-a-function-every-x-seconds-in-python
+class RepeatedTimer(object):
+    def __init__(self, interval, function, *args, **kwargs):
+        self._timer     = None
+        self.interval   = interval
+        self.function   = function
+        self.args       = args
+        self.kwargs     = kwargs
+        self.is_running = False
+        # self.start()
+
+    def _run(self):
+        self.is_running = False
+        self.start()
+        self.function(*self.args, **self.kwargs)
+
+    def set_interval(self, interval):
+        self.interval = interval
+        if self.is_running:
+            self._timer.cancel()
+        self._timer = Timer(self.interval, self._run)
+        if self.is_running:
+            self._timer.start()
+
+    def start(self):
+        if not self.is_running:
+            self._timer = Timer(self.interval, self._run)
+            self._timer.start()
+            self.is_running = True
+
+    def stop(self):
+        self._timer.cancel()
+        self.is_running = False
+
+def _bpm_call_rate_to_interval(bpm, calls_per_beat):
+    return 60 / bpm / calls_per_beat
+
+# Wrapper based on bpm
+class MeteredTimer(RepeatedTimer):
+
+    def __init__(self, bpm, calls_per_beat, function, *args, **kwargs):
+        self.calls_per_beat = calls_per_beat
+        self.bpm            = bpm
+        self.interval       = _bpm_call_rate_to_interval(bpm, calls_per_beat)
+        RepeatedTimer.__init__(self, self.interval, function, *args, **kwargs)
+
+    def set_bpm(self, bpm):
+        self.bpm = bpm
+        self.set_interval(_bpm_call_rate_to_interval(bpm, self.calls_per_beat))
+
+    def set_call_freq(self, bpm, calls_per_beat):
+        self.calls_per_beat = calls_per_beat
+        self.bpm            = bpm
+        self.set_interval(_bpm_call_rate_to_interval(bpm, calls_per_beat))
