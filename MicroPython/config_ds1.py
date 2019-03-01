ead998719fc9b55100fc1c8d0b857fbc5c71654d
MontyHallTufts <Monty.Hall.P123@gmail.com>
2019-02-28 19:00:00 -0500

    Updated drumstick upython for accelerometer

--- MicroPython/config_ds1.py
+++ MicroPython/config_ds1.py
@@ -4,10 +4,16 @@ instrument_type = 'drumstick'
 
 id = 0
 
-threshold = 500
+threshold = 250
 
 # Milliseconds between readings
-sample_frequency = 50
+sample_frequency = 10
+
+# Power to raise jerk to
+power = 2 / 3
+
+# Debounce samples
+debounce_samples = 3
 
 # Development mode. False means start read loop at boot
 dev = False
--- MicroPython/drumstick.py
+++ MicroPython/drumstick.py
@@ -22,6 +22,8 @@ import machine
 from umqtt.simple import MQTTClient
 from accelerometer import Accelerometer
 import config
+import array
+from rollingaverage import RollingStats
 
 drumstick_id = config.id
 
@@ -36,6 +38,15 @@ striking = False
 
 g_thresh = config.threshold
 
+queue_len = 100
+hit_queue = array.array("H", [0] * queue_len)
+hit_idx = 0
+hit_count = 0
+strike_avg = RollingStats(10, 10)
+debounce_count = 0
+
+prev_x, prev_y, prev_z = 0, 0, 0
+
 # For debugging. Allows dev to adjust touch_thresh in REPL
 def set_touch_thresh(new_thresh):
     global g_thresh
@@ -44,18 +55,51 @@ def set_touch_thresh(new_thresh):
 # Called when new stick struck
 def strike(stick=0, velocity=0):
     global striking
-    print("STRIKE", velocity)
+    global debounce_count
+    print("STRIKE {:5.0f}".format(velocity))
     striking = True
+    debounce_count = config.debounce_samples
+    strike_avg.update(velocity)
+    print("AVG{:5.0f}".format(strike_avg.avg))
     # client.publish('i/d/{}/d/s/{}'.format(drumpad_id, 0), str(min(velocity, 127)))
 
 # Read sesnors and update pads touched. Meant to be called frequently
 def sample(verbose=False):
     global striking
+    global hit_idx
+    global hit_queue
+    global hit_count
+    global queue_len
+    global prev_x, prev_y, prev_z
+    global debounce_count
     x, y, z = stick.get_accel()
-    val = x*x + y*y + z*z
-    if verbose:
+    dx, dy, dz = (x - prev_x), (y - prev_y), (z - prev_z)
+    val = (dx*dx + dy*dy + dz*dz) ** config.power
+    val = (dy*dy) ** config.power
+
+    # print("================================{:5.0f}".format(val))
+    # if verbose:
+    print("{:5.2f}  |  {:4.4f}, {:4.4f}, {:4.4f}".format(val, dx, dy, dz))
+    if val > config.threshold and not striking and debounce_count == 0:
         print("{:5.2f}  |  {:4.4f}, {:4.4f}, {:4.4f}".format(val, x, y, z))
-    if val > config.threshold and not striking:
-        strike(0, val * 2)
+        print("================================", val)
+        strike(0, val)
+        if (not hit_queue[hit_idx % queue_len]):
+            hit_count += 1
+            hit_queue[hit_idx % queue_len] = 1
     elif val <= config.threshold and striking:
         striking = False
+        if (hit_queue[hit_idx % queue_len]):
+            hit_count -= 1
+            hit_queue[hit_idx % queue_len] = 0
+    else:
+        if (hit_queue[hit_idx % queue_len]):
+            hit_count -= 1
+            hit_queue[hit_idx % queue_len] = 0
+    queue_time = queue_len * config.sample_frequency
+    bpm = (hit_count / queue_time) * 60000
+    # print("==============================   ", bpm)
+    hit_idx += 1
+    prev_x, prev_y, prev_z = x, y, z
+    if debounce_count > 0:
+        debounce_count -= 1
